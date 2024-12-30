ea3c857409c9777199a80a5f28856e01c0178684
SelvaAjeethS <ajeeth10gc@gmail.com>
2024-12-30 20:11:35 +0530

    Intial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import { AvatarCreator, AvatarCreatorConfig, AvatarExportedEvent } from '@readyplayerme/react-avatar-creator';
+import { Avatar } from "@readyplayerme/visage";
+import { useState } from "react";
+
+const config: AvatarCreatorConfig = {
+  clearCache: true,
+  bodyType: 'fullbody',
+  quickStart: false,
+  language: 'en',
+};
+
+const style = { width: '100%', height: '100vh', border: 'none' };
+
+export default function App() {
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const handleOnAvatarExported = (event: AvatarExportedEvent) => {
+    setAvatarUrl(event.data.url);
+  };
+
+  return (
+      <>
+        <AvatarCreator subdomain="demo" config={config} style={style} onAvatarExported={handleOnAvatarExported} />
+        {avatarUrl && <Avatar modelSrc={avatarUrl} />}
+      </>
+  );
+}
